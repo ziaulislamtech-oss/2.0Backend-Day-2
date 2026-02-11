f8ea07875a3c8f9c840567a4e1981ad15d1437c6
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-02-11 07:33:20 +0500

    comment

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express  = require('express')
-const app =  express()
-app.get('/',(req,res)=>{
-    res.send('hello world')
-})
-app.get('/about',(req,res)=>{
-    res.send('this is about page')
-})
-app.get('/home',(req,res)=>{
-    res.send('welcom home')
-})
-app.listen(3000) // server start karna
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/about', (req, res) => {
+  res.send('this is about page')
+})
+
+app.get('/home', (req, res) => {
+  res.send('welcome home')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
